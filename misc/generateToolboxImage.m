1e60aa3dff3ca70e54ff395e9774346da38603b8
Steve Eddins <steve.eddins@mathworks.com>
2018-03-19 17:18:24 -0400

    Add new toolbox image and function to generate it

---
+++ misc/generateToolboxImage.m
@@ -0,0 +1,12 @@
+function generateToolboxImage
+
+[x,y] = ndgrid(-10:.02:10);
+theta = atan2(y,x);
+rho = hypot(x,y);
+X = im2uint8(mat2gray(theta));
+rgb = ind2rgb(X,parula(256));
+rgb_256 = imresize(rgb,[256 256]);
+alpha_mask = (rho <= 10);
+alpha_mask = conv2(double(alpha_mask),ones(3,3)/9,'same');
+alpha_mask_256 = imresize(alpha_mask,[256 256]);
+imwrite(rgb_256,'color-toolbox-image.png','Alpha',alpha_mask_256)
