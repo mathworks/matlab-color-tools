eaa58a8f934ee37ccb4d08004e74d58f72af2b86
Steve Eddins <steve@eddins.net>
2019-11-08 18:21:51 -0500

    Add functions written for Digital Image Processing Using MATLAB, 3rd edition

--- tbx/Contents.m
+++ tbx/Contents.m
@@ -2,7 +2,18 @@
 % Version 1.0 (R2019b) 30-Nov-2019
 %
 % Functions
-%   ciecam022xyz   - Convert CIECAM02 color appearance values to XYZ.
-%   xyz2ciecam02   - Convert XYZ color values to CIECAM02.
+%   chromaticityDiagram    - Plot chromaticity diagram.
+%   ciecam022xyz           - Convert CIECAM02 color appearance values to XYZ.
+%   colorMatchingFunctions - Color-matching functions.
+%   colorSwatches          - Display a set of colors on individual squares.
+%   iceControlPanel        - Interactive Color Editor App.
+%   intline                - Integer-coordinate line drawing algorithm.
+%   lambda2xyz             - Convert wavelength to tristimulus values.
+%   rspd2xyz               - Convert relative spectral power density to XYZ.
+%   spectrumBar            - Add visible light spectrum bar to plot.
+%   spectrumColors         - RGB colors corresponding to the visible light spectrum.
+%   xyy2xyz                - Convert chromaticity coordinates to XYZ tristimulus values.
+%   xyz2ciecam02           - Convert XYZ color values to CIECAM02.
+%   xyz2xyy                - Convert XYZ tristimulus values to chromaticity coordinates.
 %
 %  Copyright 2017-2019 The MathWorks, Inc.
