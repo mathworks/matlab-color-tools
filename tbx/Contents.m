d1d28e82e5642c862698bfcc2c7eb5cbc9b3a568
Steve Eddins <eddins@mathworks.com>
2020-03-03 18:32:15 -0500

    Add squareColorSwatches

--- tbx/Contents.m
+++ tbx/Contents.m
@@ -1,5 +1,5 @@
 % MATLAB(R) Color Tools
-% Version 1.0 (R2019b) 30-Nov-2019
+% Version 1.1 (R2019b) 03-Mar-2020
 %
 % Functions
 %   chromaticityDiagram    - Plot chromaticity diagram.
@@ -10,10 +10,11 @@
 %   intline                - Integer-coordinate line drawing algorithm.
 %   lambda2xyz             - Convert wavelength to tristimulus values.
 %   rspd2xyz               - Convert relative spectral power density to XYZ.
+%   squareColorSwatches    - Display a set of color squares with equal aspect ratio.
 %   spectrumBar            - Add visible light spectrum bar to plot.
 %   spectrumColors         - RGB colors corresponding to the visible light spectrum.
 %   xyy2xyz                - Convert chromaticity coordinates to XYZ tristimulus values.
 %   xyz2ciecam02           - Convert XYZ color values to CIECAM02.
 %   xyz2xyy                - Convert XYZ tristimulus values to chromaticity coordinates.
 %
-%  Copyright 2017-2019 The MathWorks, Inc.
+%  Copyright 2017-2020 The MathWorks, Inc.
